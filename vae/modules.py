c147eb0185361477d2fb38d394ef35e7c35438c2
tobias teule <tobiasteule@gmail.com>
2021-01-18 15:32:54 +0100

    added stuff to modules.py

--- vae/modules.py
+++ vae/modules.py
@@ -51,7 +51,7 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
         def __init__(self):
             super(Encoder, self).__init__()
-    
+            self.lin = nn.Linear(64, 512),
             self.conv4 = nn.ConvTranspose2d(1, 16, kernel_size=3, stride=1, padding=1, bias=False)
             self.bn4 = nn.BatchNorm2d(16)
             self.conv3 = nn.ConvTranspose2d(16, 32, kernel_size=3, stride=2, padding=1, bias=False)
@@ -79,6 +79,8 @@ class Decoder(nn.Module):
     
     
         def forward(self, x):
+            x = self.lin(x)
+            x = x.reshape(128,32,16)
             conv1 = self.relu(self.bn1(self.conv1(x)))
             conv2 = self.relu(self.bn2(self.conv2(conv1)))
             conv3 = self.relu(self.bn3(self.conv3(conv2)))
